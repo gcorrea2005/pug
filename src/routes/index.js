476bc8896130f5679b9353d43e7d452e137348f9
Jorge Correa <gcorrea2005@gmail.com>
2021-02-18 08:10:06 -0500

    First Commit

--- src/routes/index.js
+++ src/routes/index.js
@@ -9,4 +9,10 @@ router.get('/contact', (req, res) => {
     res.render('contact.html', {title: 'Page Contact' });
 });
 
+// Handle 404
+router.use(function(req, res, next) {
+    res.status(404);
+    res.send('404: File Not Found');
+});
+
 module.exports = router;
